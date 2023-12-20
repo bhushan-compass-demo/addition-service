d8eabe0ad95daa86249ea7d77539b2796fe6451d
Bhushan Nagaraj <bnagaraj@atlassian.com>
2023-12-20 12:32:03 +1100

    AD-2 Code repository created for addition service

---
+++ src/test/java/ut/com/devdemo/AdditionServiceUnitTests.java
@@ -0,0 +1,35 @@
+package ut.com.devdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AdditionServiceUnitTests
+{
+    @Test
+    public void testAdditionService()
+    {
+        Integer sum = 10;
+        assertEquals(10, sum.intValue());
+    }
+
+    @Test
+    public void testAdditionService2()
+    {
+        Integer sum = 10;
+        assertEquals(10, sum.intValue());
+    }
+
+    @Test
+    public void testAdditionService3()
+    {
+        Integer sum = 10;
+        assertEquals(10, sum.intValue());
+    }
+    
+    public void testAdditionService4()
+    {
+        Integer sum = 10;
+        assertEquals(10, sum.intValue());
+    }
+}
